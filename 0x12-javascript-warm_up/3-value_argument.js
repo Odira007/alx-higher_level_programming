cf210ccb3baae9a2df150fd0034bb87eab0ebb71
Odira Ikewelugo <odiraikewelugo@gmail.com>
2023-03-15 14:55:10 -0700

    JavaScript Warm Up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const numArgs = process.argv.length - 2;
 if (numArgs === 0) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log(process.argv[2]);
+  console.log(process.argv[2]);
 }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-let firstArg = parseInt(process.argv[2]);
-let secondArg = parseInt(process.argv[3]);
+const firstArg = parseInt(process.argv[2]);
+const secondArg = parseInt(process.argv[3]);
 
 function add (a, b) {
-	return a + b;
+  return a + b;
 }
 console.log(add(firstArg, secondArg));
-
