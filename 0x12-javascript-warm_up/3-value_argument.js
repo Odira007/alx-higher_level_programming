442ed249dc2061b3918c3fa858ffa9466b9a7673
Odira Ikewelugo <odiraikewelugo@gmail.com>
2023-03-15 15:14:56 -0700

    JavaScript Warm Up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const numArgs = process.argv.length - 2;
-if (numArgs === 0) {
-  console.log('No argument');
+const firstArg = process.argv[2];
+if (firstArg === undefined) {
+    console.log('No argument');
 } else {
-  console.log(process.argv[2]);
+    console.log(firstArg);
 }
