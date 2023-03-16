fd28071366d1c185b77812b645ddb23e1dac44e5
Odira Ikewelugo <odiraikewelugo@gmail.com>
2023-03-16 09:51:09 +0100

    Learning JavaScript

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -5,20 +5,16 @@ module.exports = class Rectangle {
     this.width = w;
     this.height = h;
   }
-
   print (char = 'X') {
     for (let i = 0; i < this.height; i++) {
 	    console.log(char.repeat(this.width));
     }
   }
-
   rotate () {
     [this.height, this.width] = [this.width, this.height];
   }
-
   double () {
     this.height = this.height * 2;
-
     this.width = this.width * 2;
   }
 };
