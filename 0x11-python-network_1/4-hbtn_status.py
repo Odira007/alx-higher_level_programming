924436e1e02d260fd9de0c551bb92e1e8fa9a8ce
Odira Ikewelugo <odiraikewelugo@gmail.com>
2023-03-31 19:18:20 -0700

    Python Network 2

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
+"""Use requests package to make a get request to given URL
 """
-Use requests package to make a get request to given URL
-"""
-import sys
 import requests
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-    response = requests.get(url)
+    r = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
