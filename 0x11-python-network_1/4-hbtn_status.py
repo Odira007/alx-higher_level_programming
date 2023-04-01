a6a914a9102560f4f37fe26171639ea55c8854f8
Odira Ikewelugo <odiraikewelugo@gmail.com>
2023-03-31 18:56:36 -0700

    Python Network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
     response = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
+    print("    - type: {}".format(type(response.text)))
+    print("    - content: {}".format(response.text))
