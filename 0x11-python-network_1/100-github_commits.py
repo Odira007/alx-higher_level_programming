769a36acad13f10afbaa6bb2db8e21d191091cf3
Odira Ikewelugo <odiraikewelugo@gmail.com>
2023-03-31 18:05:53 -0700

    Python Network 2

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,9 +11,9 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
             sys.argv[2], sys.argv[1])
     r = requests.get(url)
-    commits = r.json()
+    c = r.json()
     try:
         for i in range(10):
-            print(f"{commits[i]['sha']}: {commits[i]['commit']['author']['name']}")
+            print(f"{c[i]['sha']}:{c[i]['commit']['author']['name']}")
     except IndexError:
         pass
