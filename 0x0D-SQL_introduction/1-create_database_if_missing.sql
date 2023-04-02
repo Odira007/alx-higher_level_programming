75465ab4d1acbd96a078631a5f5a6120115e03e6
Odira Ikewelugo <odiraikewelugo@gmail.com>
2023-04-02 04:09:59 -0700

    SQL introduction

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- script that creates the database hbtn_0c_0
+-- create a new database
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- deletes the database hbtn_0c_0
+-- delete the last created database
+DROP DATABASE IF EXISTS hbtn_0c_0
